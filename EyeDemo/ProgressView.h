4f5df0cf0b6ce65a4a344aca7eb9ec91f445a557
路亮亮 <luliangliang@luliangliangdeMac-mini.local>
2016-03-26 11:25:27 +0800

    slider

--- EyeDemo/ProgressView.h
+++ EyeDemo/ProgressView.h
@@ -7,10 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#define RGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16)) / 255.0 green:((float)((rgbValue & 0xFF00) >> 8)) / 255.0 blue:((float)(rgbValue & 0xFF)) / 255.0 alpha:1.0]
-#define APP_WIDTH [UIScreen mainScreen].bounds.size.width
-#define APP_HEIGHT [UIScreen mainScreen].bounds.size.height
-
+#import "TDHeader.h"
 /**
  *  视频录制中的进度条
  */
